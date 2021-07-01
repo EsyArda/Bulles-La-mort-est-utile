adaa6d937c66d458f3200c55e08c9cc4fcf2e2c8
Lilian Hiault <34962237+LilianHiault@users.noreply.github.com>
2021-07-01 23:55:46 +0200

    Musique niveau 4

--- Levels/level4.gd
+++ Levels/level4.gd
@@ -1,7 +1,7 @@
 extends "res://Game.gd"
 const level_Name  := "Level 4"
 export var  spawn_player := Vector2(-200,136)
-const next_level := "res://levels/level1.tscn"
+const next_level := "res://Main menu.tscn"
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
